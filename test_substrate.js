63a36f2696231817f95ff26801f093dea93c6b79
zengsheng <roman.zengsheng@gmail.com>
2022-07-29 15:52:11 +0800

    tidy up code about substrate content

--- test_substrate.js
+++ test_substrate.js
@@ -1,21 +1,21 @@
-//let crypto = require('crypto');  //加载crypto库
-//console.log(crypto.getHashes()); //打印支持的hash算法
-//console.log(crypto.getCiphers());
-//console.log(crypto.getCurves());
-
 const bs58 = require('bs58')
 let blake2 = require('blake2');
+const base58 = require("./base58");
 
-//for substract address calculation
+let public_key = "54902ac86eb3bc2e1ecf30533e2d9f1f1b034ac0dd35ff41f44da47384646169";
+console.log("public key", public_key);
+
 let prefix = "00";
-let pubkey = "54902ac86eb3bc2e1ecf30533e2d9f1f1b034ac0dd35ff41f44da47384646169";
-let data = prefix + pubkey;
+console.log("use prefix:", prefix);
+
+let data = prefix + public_key;
 
 let h = blake2.createHash('blake2b');
 h.update(Buffer.from('53533538505245'+ data, 'hex'));
 let checksum = h.digest("hex").substring(0, 4);
 console.log("checksum:", checksum);
+
 const bytes = Buffer.from(data + checksum, 'hex')
 const address = bs58.encode(bytes)
-console.log("address:", address);
-
+const address2 = base58.encode58(bytes);
+console.log("substrate address:", address, address2);
