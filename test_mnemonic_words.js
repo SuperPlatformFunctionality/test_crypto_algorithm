982c7d94053cd86843a5a145dfe7fae39bc64370
RomiJones <roman.zengsheng@gmail.com>
2023-01-16 08:01:19 +0800

    tidy up some code

--- test_mnemonic_words.js
+++ test_mnemonic_words.js
@@ -1,15 +1,17 @@
 const bip39 = require("bip39");
 const ethers = require("ethers");
 
-// Generate a random mnemonic (uses crypto.randomBytes under the hood), defaults to 128-bits of entropy
-let mnemonic = bip39.generateMnemonic()
-console.log(mnemonic);
-console.log(bip39.validateMnemonic(mnemonic));  //test the mnemonic word validation
+// generate a random mnemonic (uses crypto.randomBytes under the hood), defaults to 128-bits of entropy
+let mnemonic = bip39.generateMnemonic();
+console.log("mnemonic : ", mnemonic);
+console.log("is valid : ", bip39.validateMnemonic(mnemonic));  //test the mnemonic word validation
 
-//let seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
-//console.log(seed);
+//
+let seed = bip39.mnemonicToSeedSync("pond across joke machine pink input letter couch lizard safe body squeeze").toString('hex');
+console.log("seed:", seed);
 
-//load mnemonic word
+// load mnemonic word
 let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);  //use default path m/44’/60’/0’/0/0
-console.log(mnemonicWallet.address);
-console.log(mnemonicWallet.privateKey);
+console.log("private key:", mnemonicWallet.privateKey);
+console.log("address:", mnemonicWallet.address);
+
