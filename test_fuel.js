2f3572592ccb38ed2ea6aa1f38c628756a9d2658
zengsheng <zengsheng@zengshengdeMacBookMarket.local>
2024-05-07 00:02:04 +0800

    add some materials about fuel

---
+++ test_fuel.js
@@ -0,0 +1,16 @@
+const walletKey = require("./walletKey.js");
+const fuels = require("fuels");
+
+// Random Wallet
+//console.log(fuels.Wallet.generate());
+
+// Using privateKey Wallet
+let priKey = walletKey.priKey;
+console.log("priKey is", priKey);
+
+let theWalletUnlocked = fuels.Wallet.fromPrivateKey(priKey);
+console.log(theWalletUnlocked);
+console.log(theWalletUnlocked.address.toB256());
+
+let theWalletLocked = fuels.Wallet.fromAddress(theWalletUnlocked.address);
+console.log(theWalletLocked);
